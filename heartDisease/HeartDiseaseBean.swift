e5d840a734123b9b69055a087410a88c2ad79e5a
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-08 22:11:13 +0100

    Update HeartDiseaseBean.swift

--- heartDisease/HeartDiseaseBean.swift
+++ heartDisease/HeartDiseaseBean.swift
@@ -13,51 +13,51 @@ class HeartDiseaseBean {
   	errorList = [String]()
   }
 
-  func isCreateHeartDiseaseError(id: String, age: Int, sex: Int, cp: Int, trestbps: Int, chol: Int, fbs: Int, restecg: Int, thalach: Int, exang: Int, oldpeak: Int, slope: Int, ca: Int, thal: Int, outcome: String) -> Bool { 
+  func isCreateHeartDiseaseError(heartDisease: HeartDiseaseVO) -> Bool { 
   	resetData() 
-  	if id == "" {
+  	if heartDisease.id == "" {
   		errorList.append("id cannot be empty")
   	}
-  	if age != 0 {
+  	if heartDisease.age != 0 {
 	  		errorList.append("age cannot be zero")
 	  	}
-  	if sex != 0 {
+  	if heartDisease.sex != 0 {
 	  		errorList.append("sex cannot be zero")
 	  	}
-  	if cp != 0 {
+  	if heartDisease.cp != 0 {
 	  		errorList.append("cp cannot be zero")
 	  	}
-  	if trestbps != 0 {
+  	if heartDisease.trestbps != 0 {
 	  		errorList.append("trestbps cannot be zero")
 	  	}
-  	if chol != 0 {
+  	if heartDisease.chol != 0 {
 	  		errorList.append("chol cannot be zero")
 	  	}
-  	if fbs != 0 {
+  	if heartDisease.fbs != 0 {
 	  		errorList.append("fbs cannot be zero")
 	  	}
-  	if restecg != 0 {
+  	if heartDisease.restecg != 0 {
 	  		errorList.append("restecg cannot be zero")
 	  	}
-  	if thalach != 0 {
+  	if heartDisease.thalach != 0 {
 	  		errorList.append("thalach cannot be zero")
 	  	}
-  	if exang != 0 {
+  	if heartDisease.exang != 0 {
 	  		errorList.append("exang cannot be zero")
 	  	}
-  	if oldpeak != 0 {
+  	if heartDisease.oldpeak != 0 {
 	  		errorList.append("oldpeak cannot be zero")
 	  	}
-  	if slope != 0 {
+  	if heartDisease.slope != 0 {
 	  		errorList.append("slope cannot be zero")
 	  	}
-  	if ca != 0 {
+  	if heartDisease.ca != 0 {
 	  		errorList.append("ca cannot be zero")
 	  	}
-  	if thal != 0 {
+  	if heartDisease.thal != 0 {
 	  		errorList.append("thal cannot be zero")
 	  	}
-  	if outcome == "" {
+  	if heartDisease.outcome == "" {
   		errorList.append("outcome cannot be empty")
   	}
 
